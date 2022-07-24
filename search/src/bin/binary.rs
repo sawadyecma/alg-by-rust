15b9c290ba0c36b3a8b697553da2776b45cb97d9
sawadyecma <snowslidez@yahoo.co.jp>
2022-07-24 11:02:22 +0900

    add test_search

--- search/src/bin/binary.rs
+++ search/src/bin/binary.rs
@@ -1,21 +1,13 @@
 fn main() {
     println!("[BINARY SEARCH]");
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9);
-    process(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10);
+    search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0);
 }
 
-fn process(slice: &[i32], tar: i32) {
+pub fn search(slice: &[i32], tar: i32) -> usize {
     let result = binary_search(&slice, tar);
     println!("[OUTPUT] index of {} is ... {}", tar, result.unwrap_or(999));
+
+    result.unwrap_or(999)
 }
 
 fn binary_search(slice: &[i32], tar: i32) -> Option<usize> {
@@ -38,5 +30,22 @@ fn binary_search(slice: &[i32], tar: i32) -> Option<usize> {
         if st == ed {
             return None;
         }
+    }
+}
+
+mod tests {
+    #[test]
+    fn test_search() {
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0), 999);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1), 0);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2), 1);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3), 2);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4), 3);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5), 4);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6), 5);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7), 6);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8), 7);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9), 8);
+        assert_eq!(crate::search(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10), 9);
     }
 }
